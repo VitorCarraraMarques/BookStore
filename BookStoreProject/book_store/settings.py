2c222fd34a4d02e2da6f4baa5c2a5515f62405ce
Vítor Carrara <vitorcarrara1998@gmail.com>
2023-02-06 14:34:00 -0300

    Correct typo

--- BookStoreProject/book_store/settings.py
+++ BookStoreProject/book_store/settings.py
@@ -135,6 +135,6 @@ INTERNAL_IPS = [
 
 
 REST_FRAMEWORK = {
-    'DEFAUTL_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 
     'PAGE_SIZE': 5
 }
