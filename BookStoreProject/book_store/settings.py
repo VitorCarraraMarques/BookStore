1eddb00056239cf0ec4973aae38947b5b43a272d
Vítor Carrara <vitorcarrara1998@gmail.com>
2023-02-07 11:32:02 -0300

    Change secret_key, debug and allowed_hosts env variables

--- BookStoreProject/book_store/settings.py
+++ BookStoreProject/book_store/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^4!-%yja7o8bbk+h7o^r+^2!9=(_7ra)(g$si%nx10s=rkl7*p'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(" ")
 
 
 # Application definition
