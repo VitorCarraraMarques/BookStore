abb63fb62e82918e74a4d15d5e6b865d13c0c24d
Vítor Carrara <vitorcarrara1998@gmail.com>
2023-02-09 13:24:37 -0300

    Hard code the value of SECRET_KEY (sic)

--- book_store/settings.py
+++ book_store/settings.py
@@ -146,7 +146,7 @@ REST_FRAMEWORK = {
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'foo' #os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
