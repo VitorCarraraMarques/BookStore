ac435f872b0f0fa03903b16e4d43addeb4cb00cf
Vítor Carrara <vitorcarrara1998@gmail.com>
2023-02-09 11:31:12 -0300

    Comment out authentication parameters wrongly left on the code

--- BookStoreProject/product/viewsets/product_viewset.py
+++ BookStoreProject/product/viewsets/product_viewset.py
@@ -1,13 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
 #from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+#from rest_framework.permissions import IsAuthenticated
 
 from product.serializers.product_serializer import ProductSerializer
 from product.models import Product
 
 class ProductViewSet(ModelViewSet):
     #authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    #permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
     
     def get_queryset(self):
